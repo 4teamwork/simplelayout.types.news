05e0535946f2204bcc1feefa3c201eb05464f87b
Julian Infanger <j.infanger@4teamwork.ch>
2012-04-30 11:39:40 +0000

    Added default date for news.

--- simplelayout/types/news/news.py
+++ simplelayout/types/news/news.py
@@ -1,20 +1,21 @@
+from DateTime import DateTime
 from simplelayout.types.common.content.simplelayout_schemas import imageSchema
 from simplelayout.types.common.content import page
 from simplelayout.types.common.content.page import Page
 
 from AccessControl import ClassSecurityInfo
-from Products.Archetypes.atapi import *
-from simplelayout.types.news.config import *
+from Products.Archetypes import atapi
+from simplelayout.types.news.config import PROJECTNAME
 from simplelayout.types.common.content.simplelayout_schemas import finalize_simplelayout_schema
 from zope.interface import implements
 from simplelayout.types.news.interfaces import INews
 from simplelayout.base.interfaces import ISimpleLayoutCapable
 
-from simplelayout.types.news import _
 
 news_schema = page.page_schema.copy() + imageSchema.copy()
 
 news_schema['effectiveDate'].required = True
+news_schema['effectiveDate'].default_method = 'getDefaultEffectiveDate'
 
 finalize_simplelayout_schema(news_schema, folderish=True)
 
@@ -25,6 +26,10 @@ class News(Page):
 
     schema = news_schema
 
+    def getDefaultEffectiveDate(self):
+        return DateTime().Date()
 
-registerType(News, PROJECTNAME)
+
+
+atapi.registerType(News, PROJECTNAME)
 
