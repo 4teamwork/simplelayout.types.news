c98bb1bc4cb622fd7b3553a8d9b5575d0b26072a
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-12-12 13:36:05 +0100

    Code cleanup vol. 2

--- simplelayout/types/news/tests/test_news_portlet.py
+++ simplelayout/types/news/tests/test_news_portlet.py
@@ -1,15 +1,15 @@
 from ftw.testing import MockTestCase
 from simplelayout.types.news.portlets.news_portlet import Assignment, Renderer
 from mocker import MATCH
-class TestNewsPortlet(MockTestCase):
 
+
+class TestNewsPortlet(MockTestCase):
 
     def test_onlycontext(self):
 
         def query_matcher(query):
             self.assertEqual(query['path']['query'], 'folder/file')
             return True
-
 
         context = self.mocker.mock()
         request = self.mocker.mock()
@@ -21,12 +21,12 @@ class TestNewsPortlet(MockTestCase):
         self.mock_tool(url, 'portal_url')
         self.expect(url.getPortalPath()).result('/plone')
         self.expect(context.getPhysicalPath()).result(('folder', 'file'))
-        self.expect(catalog.searchResults(MATCH(query_matcher))).count(1).result(['1', '2', '3', '4', '5'])
+        self.expect(catalog.searchResults(
+            MATCH(query_matcher))).count(1).result(['1', '2', '3', '4', '5'])
         self.replay()
         obj = Assignment(path=['/peter/hans'])
         renderer = Renderer(context, request, view, manager, obj)
         renderer.get_news()
-
 
     def test_path(self):
 
@@ -34,7 +34,6 @@ class TestNewsPortlet(MockTestCase):
             self.assertEqual(query['path']['query'], ['/plone/peter/hans'])
             return True
 
-
         context = self.mocker.mock()
         request = self.mocker.mock()
         view = self.mocker.mock()
@@ -44,13 +43,14 @@ class TestNewsPortlet(MockTestCase):
         self.mock_tool(catalog, 'portal_catalog')
         self.mock_tool(url, 'portal_url')
         self.expect(url.getPortalPath()).result('/plone')
-        self.expect(context.getPhysicalPath()).result(('folder', 'file')).count(0, None)
-        self.expect(catalog.searchResults(MATCH(query_matcher))).count(1).result(['1', '2', '3', '4', '5'])
+        self.expect(context.getPhysicalPath()).result(
+            ('folder', 'file')).count(0, None)
+        self.expect(catalog.searchResults(
+            MATCH(query_matcher))).count(1).result(['1', '2', '3', '4', '5'])
         self.replay()
         obj = Assignment(path=['peter/hans'], only_context=False)
         renderer = Renderer(context, request, view, manager, obj)
         renderer.get_news()
-
 
     def test_subject(self):
 
@@ -58,7 +58,6 @@ class TestNewsPortlet(MockTestCase):
             self.assertEqual(query['Subject'], ['hans', 'peter'])
             return True
 
-
         context = self.mocker.mock()
         request = self.mocker.mock()
         view = self.mocker.mock()
@@ -68,8 +67,10 @@ class TestNewsPortlet(MockTestCase):
         self.mock_tool(catalog, 'portal_catalog')
         self.mock_tool(url, 'portal_url')
         self.expect(url.getPortalPath()).result('/plone')
-        self.expect(context.getPhysicalPath()).result(('folder', 'file')).count(0, None)
-        self.expect(catalog.searchResults(MATCH(query_matcher))).count(1).result(['1', '2', '3', '4', '5'])
+        self.expect(context.getPhysicalPath()).result(
+            ('folder', 'file')).count(0, None)
+        self.expect(catalog.searchResults(
+            MATCH(query_matcher))).count(1).result(['1', '2', '3', '4', '5'])
         self.replay()
         obj = Assignment(subjects=['hans', 'peter'], only_context=False)
         renderer = Renderer(context, request, view, manager, obj)
@@ -80,7 +81,6 @@ class TestNewsPortlet(MockTestCase):
         def query_matcher(query):
             self.assertEqual(query['cs_uids'], ['1234567'])
             return True
-
 
         context = self.mocker.mock()
         request = self.mocker.mock()
@@ -93,10 +93,14 @@ class TestNewsPortlet(MockTestCase):
         self.mock_tool(url, 'portal_url')
         self.expect(obj_uid.UID()).result('1234567')
         self.expect(url.getPortalPath()).result('/plone')
-        self.expect(context.restrictedTraverse('/plone/hans/peter')).result(obj_uid)
-        self.expect(context.getPhysicalPath()).result(('folder', 'file')).count(0, None)
-        self.expect(catalog.searchResults(MATCH(query_matcher))).count(1).result(['1', '2', '3', '4', '5'])
+        self.expect(context.restrictedTraverse(
+            '/plone/hans/peter')).result(obj_uid)
+        self.expect(context.getPhysicalPath()).result(
+            ('folder', 'file')).count(0, None)
+        self.expect(catalog.searchResults(
+            MATCH(query_matcher))).count(1).result(['1', '2', '3', '4', '5'])
         self.replay()
-        obj = Assignment(classification_items=['hans/peter'], only_context=False)
+        obj = Assignment(classification_items=['hans/peter'],
+                         only_context=False)
         renderer = Renderer(context, request, view, manager, obj)
         renderer.get_news()
