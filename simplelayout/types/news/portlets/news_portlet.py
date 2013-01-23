49fc444b20893f80a9c9212c407e0537384aec60
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-01-23 19:13:18 +0100

    Implement available method - do not show portlet if there is no news

--- simplelayout/types/news/portlets/news_portlet.py
+++ simplelayout/types/news/portlets/news_portlet.py
@@ -176,6 +176,10 @@ class Renderer(base.Renderer):
             return scaled_img.tag()
         return ''
 
+    @property
+    def available(self):
+        return bool(self.get_news())
+
     def get_news(self):
         catalog = getToolByName(self.context, 'portal_catalog')
         url_tool = getToolByName(self.context, 'portal_url')
